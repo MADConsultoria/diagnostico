f37ad01bc6d1a0d83a710a9b5b751b9a41530081
MADConsultoria <comercial@madconsultoria.com.br>
2025-07-01 13:38:37 -0300

    Add files via upload

---
+++ 1-diagnostico-f/src/pages/Index.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Quiz from '../components/Quiz';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
+      <Quiz />
+    </div>
+  );
+};
+
+export default Index;
